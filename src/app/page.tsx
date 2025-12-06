c73290153ddf44fa2faa1ba0f504537df80c603d
skouza101 <skouza.101@gmail.com>
2025-12-06 19:41:27 +0100

    add Tape section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { ProjectsSection } from "@/sections/Projects";
 import { Header } from "../sections/Header";
 import { HeroSection } from "../sections/Hero";
+import { TapeSection } from "@/sections/Tape";
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
       <ProjectsSection />
+      <TapeSection />
     </div>
   );
 }
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,36 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Scalable",
+  "Responsive",
+  "Modern",
+  "Customizable",
+  "Fast",
+  "secure",
+  "search engine optimized",
+  "accessible",
+  "easy to use",
+  "user friendly",
+  "interactive",
+  "responsive",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <section className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-sky-400 to-blue-500 -rotate-3">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_80%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+          {words.map((word, index) => (
+            <div key={index} className="inline-flex gap-4 items-center">
+              <span className="text-gray-900 font-extrabold uppercase text-sm">{word}</span>
+              <StarIcon className="size-6 text-gray-900" />
+            </div>
+          ))}
+        </div>
+        </div>
+      </div>
+    </section>
+  );
 };
